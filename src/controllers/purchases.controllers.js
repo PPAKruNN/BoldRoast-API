c09d6ae96ddf4b4f521f7aefa901ac4737da1f78
Pedro Nascimento <47681197+PPAKruNN@users.noreply.github.com>
2023-07-14 17:07:01 -0300

    Style: remove whitespaces

--- src/controllers/purchases.controllers.js
+++ src/controllers/purchases.controllers.js
@@ -2,6 +2,4 @@ export async function newPurchase (req,res){
     const {userId, notes, products, adressInfo, paymentInfo} = req.body;
     const { authorization } = req.headers;
 	const token = authorization?.replace("Bearer ", "");
-
-
 }
