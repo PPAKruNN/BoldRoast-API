d17baafd8d41c6f85033c282280311bb7e453006
Pedro Nascimento <sn.jaypee@gmail.com>
2023-07-15 16:10:01 -0300

    feat: add DELETE /cart

--- src/routes/cart.routes.js
+++ src/routes/cart.routes.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import validateToken from "../middlewares/validateToken.js";
 import { validateBody } from "../middlewares/validateBody.js";
-import { getCart, updateCart } from "../controllers/cart.controllers.js";
+import { deleteCart, getCart, updateCart } from "../controllers/cart.controllers.js";
 import cartSchema from "../schemas/cartSchema.js";
 
 const cartRouter = Router();
 
 cartRouter.get("/cart", validateToken, getCart);
 cartRouter.put("/cart", validateBody(cartSchema), validateToken, updateCart); 
+cartRouter.delete("/cart", validateToken, deleteCart); 
 
 export default cartRouter;
