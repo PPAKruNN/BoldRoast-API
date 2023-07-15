a03f3ab3b47778a7bc4a32254a224e98f1ac2da4
Pedro Nascimento <sn.jaypee@gmail.com>
2023-07-15 15:12:39 -0300

    feat: add GET cart

--- src/routes/cart.routes.js
+++ src/routes/cart.routes.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
+import validateToken from "../middlewares/validateToken";
 
 const cartRouter = Router();
 
+cartRouter.get("/cart", validateToken, getCart);
 
 export default cartRouter;
