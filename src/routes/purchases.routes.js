da7556108cb5700482c9b56a6c275511e711478c
rafaelanobre <raafanobre@gmail.com>
2023-07-14 21:51:41 -0300

    :wrench: Fix: validade token in purchase route

--- src/routes/purchases.routes.js
+++ src/routes/purchases.routes.js
@@ -2,10 +2,11 @@ import { Router } from "express";
 import { newPurchase } from "../controllers/purchases.controllers.js";
 import { validateSchema } from "../middlewares/validateSchema.js";
 import { purchaseSchema } from "../schemas/purchaseSchema.js";
+import validateToken from "../middlewares/validateToken.js";
 
 const purchaseRouter = Router();
 
-purchaseRouter.post("/purchase", validateSchema(purchaseSchema), newPurchase);
+purchaseRouter.post("/purchase", validateSchema(purchaseSchema), validateToken, newPurchase);
 
 
 export default purchaseRouter;
