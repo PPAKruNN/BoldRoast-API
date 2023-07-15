99a1c7e3b612869ab78b9c2ea86a87dcf323db11
Pedro Nascimento <sn.jaypee@gmail.com>
2023-07-15 16:09:05 -0300

    feat: deleteCart

--- src/controllers/cart.controllers.js
+++ src/controllers/cart.controllers.js
@@ -4,7 +4,7 @@ export async function getCart(req, res) {
 
     try {
         const cartSearch = await db.collection("carts").findMany({userId: res.locals.userId});
-        if(cartSearch.error) res.sendStatus(404);
+        if(!cartSearch) res.sendStatus(404);
 
         res.send(cartSearch);
 
@@ -17,10 +17,21 @@ export async function updateCart(req, res) {
 
     try {
         const cartUpdate = await db.collection("carts").updateOne({userId: req.locals.userId}, {$set: {products: req.body.products}});
-        if(cartUpdate.error) return res.sendStatus(404);
+        if(cartUpdate.matchedCount === 0) return res.sendStatus(404);
 
         res.sendStatus(200);
+    } catch (error) {
+        res.status(500).send(error);        
+    }
+}
 
+export async function deleteCart(req, res) {
+
+    try {
+        const cartDelete = await db.collection("carts").deleteOne({userId: req.locals.userId});
+        if(cartDelete.deletedCount === 0) return res.sendStatus(404);
+
+        res.sendStatus(200);
     } catch (error) {
         res.status(500).send(error);        
     }
