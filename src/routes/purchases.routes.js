6661949d37f99af2e87b2bfb8d55525acb31b138
rafaelanobre <raafanobre@gmail.com>
2023-07-15 11:39:48 -0300

    :hammer: Refactor: validate schema to validate body

--- src/routes/purchases.routes.js
+++ src/routes/purchases.routes.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import { listUserPurchases, newPurchase, purchaseDetails } from "../controllers/purchases.controllers.js";
-import { validateSchema } from "../middlewares/validateSchema.js";
 import { purchaseSchema } from "../schemas/purchaseSchema.js";
 import validateToken from "../middlewares/validateToken.js";
+import { validateBody } from "../middlewares/validateBody.js";
 
 const purchaseRouter = Router();
 
-purchaseRouter.post("/purchases", validateSchema(purchaseSchema), validateToken, newPurchase);
+purchaseRouter.post("/purchases", validateBody(purchaseSchema), validateToken, newPurchase);
 purchaseRouter.get("/purchases", validateToken, listUserPurchases);
 purchaseRouter.get("/purchases/:id", validateToken, purchaseDetails);
 
