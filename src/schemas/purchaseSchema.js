dea0db96bf4d7487fb9b0496e095243eee35a430
rafaelanobre <raafanobre@gmail.com>
2023-07-17 21:44:34 -0300

    :wrench: Fix: purchase schema

--- src/schemas/purchaseSchema.js
+++ src/schemas/purchaseSchema.js
@@ -3,24 +3,19 @@ import Joi from "joi";
 export const purchaseSchema = Joi.object({
     notes: Joi.string().required().allow(""),
 
-    products: Joi.array().items(
-        Joi.object({
-            productId: Joi.string().required(),
-            productVariation: Joi.string().optional(),
-            productQuantity: Joi.number().integer().min(1).required()
-        })).required(),
+    products: Joi.array().required(),
 
     addressInfo: Joi.object({
         addressName: Joi.string().required(),
         cep: Joi.string().pattern(/^(\d{5})[-]?(\d{3})$/).required(),
         address: Joi.string().required(),
-        addressComplement: Joi.string().optional(),
+        addressComplement: Joi.string().optional().allow(""),
         city: Joi.string().required(),
         state: Joi.string().required()
         }).required(),
 
     paymentInfo: Joi.object({
-        creditCard: Joi.string().creditCard().required(),
+        creditCard: Joi.string().required(),
         cardOwner: Joi.string().required(),
         cardExpiringDate: Joi.string().pattern(/^[0-9]{2}\/[0-9]{2}$/).required(),
         cvv: Joi.string().pattern(/^[0-9]{3}$/).required(),
